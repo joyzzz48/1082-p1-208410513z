8a63bf23007b0b2e1834d394ba91671b75f15e1e
joyzzz48 <joy897346@gmail.com>
2020-04-26 12:33:07 +0800

    initial commit, saving w01~w05 files

---
+++ w2-monster/assets/scripts/app.js
@@ -0,0 +1,17 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+}
+
+attackBtn.addEventListener('click', attackHandler);
